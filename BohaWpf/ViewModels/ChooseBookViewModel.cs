137b917eece30a1074d72b7da2bd7820fd1a654b
Falko Gebel <falko-gebel@live.de>
2024-11-20 21:42:18 +0100

    Worked on ChooseBookViewModel

--- BohaWpf/ViewModels/ChooseBookViewModel.cs
+++ BohaWpf/ViewModels/ChooseBookViewModel.cs
@@ -1,29 +1,49 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using BohaLibrary;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace BohaWpf.ViewModels
 {
     public partial class ChooseBookViewModel : ObservableObject
     {
-        //private ChooseBookView _chooseBookView;
+        private readonly BooksOfHouseholdAccounts _books;
+        private readonly string _pathFiles = Application.Current.Properties["PathFiles"] as string ?? throw new ArgumentNullException(nameof(_pathFiles));
 
         [ObservableProperty]
-        private ObservableCollection<string>? _names = ["book1", "book2", "third book"];
+        private ObservableCollection<string>? _names;
 
         public ChooseBookViewModel()
         {
-            // TODO - load books from file
-
-            Names.Add("book from constructor");
+            _books = new BooksOfHouseholdAccounts();
+            LoadBooksAndUpdateNames();
         }
 
         [RelayCommand]
         private void AddBook()
         {
-            // TODO - add new book to list
+            if (Names == null)
+                throw new ArgumentNullException(nameof(Names));
 
-            Names.Add("book from AddBook");
+            try
+            {
+                // TODO - get book from input dialog
+
+                _books.AddBook("book from AddBook");
+                _books.SaveToFile(_pathFiles);
+                LoadBooksAndUpdateNames();
+            }
+            catch (ArgumentException ae)
+            {
+                MessageBox.Show(ae.Message);
+            }
+        }
+
+        private void LoadBooksAndUpdateNames()
+        {
+            _books.LoadFromFile(_pathFiles);
+            Names = new ObservableCollection<string>(_books.Names);
         }
     }
 }
